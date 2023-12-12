10b8d73f774479fa2cd39723b63d4e41f421b4f1
Jeremiah Bolden <Jeremiah791@revature.net>
2023-12-12 14:59:06 -0500

    images added fonts fixed

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 
 //entry point of the application
 const app = express();
@@ -7,9 +8,10 @@ const app = express();
 app.set('view engine', 'ejs');
 
 //middleware
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000)
+
 app.get('/', (req, res) => {
     res.render('index')
 })
