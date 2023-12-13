1f06768c95b2be29c2fea00ba9201b828e069e4f
Jeremiah Bolden <Jeremiah791@revature.net>
2023-12-12 19:58:40 -0500

    about page added, 404 page added, images added and borderized

--- src/app.js
+++ src/app.js
@@ -12,6 +12,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000)
 
+
+
 app.get('/', (req, res) => {
     res.render('index')
+})
+
+app.get('/about', (req, res) =>{
+    res.render('about')
+})
+
+//404 page
+app.use((req, res) => {
+    res.status(404).render('404')
 })
